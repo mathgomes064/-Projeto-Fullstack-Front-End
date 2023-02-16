75f1700d465ac691d318d521a97ed168cc10b0f9
Matheus_Gomes <mathgomes007@htomail.com>
2023-02-16 13:27:25 -0300

    added carousel of cars

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,10 @@
 import { Cars } from "../../components/Cars"
+import { DashboardPage } from "./style"
 
 export const Dashboard = () =>{
     return(
-        <>
+        <DashboardPage>
             <Cars/>
-        </>
+        </DashboardPage>
     )
 }
