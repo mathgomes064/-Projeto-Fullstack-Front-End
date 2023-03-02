3afb680e0ba0439de1d3dc788f2d78ee30007509
nicoleantuness <nicoleantunes2016@gmail.com>
2023-03-02 15:58:11 -0300

    implementing hook form

--- src/components/LoginCard/index.tsx
+++ src/components/LoginCard/index.tsx
@@ -1,20 +1,47 @@
 import { Container } from "./style";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
 
 export const LoginCard = () => {
+  const schema = yup.object().shape({
+    email: yup.string().required("Email é obrigatório"),
+    password: yup.string().required("Senha é obrigatório"),
+  });
+//asafasdfgsdfgtestetet
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitFunction = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <Container>
       <h1 id="title-5-500">Login</h1>
       <div id="wrapper-form">
-        <form action="submit">
+        <form onSubmit={handleSubmit(onSubmitFunction)}>
           <label>Usuário</label>
-          <input type="text" placeholder="Digitar usuário" />
+          <input
+            type="text"
+            placeholder="Digitar usuário"
+            {...register("email")}
+          />
           <label>Senha</label>
-          <input type="text" placeholder="Digitar senha" />
+          <input
+            type="password"
+            placeholder="Digitar senha"
+            {...register("password")}
+          />
+          <a id="text-2-500">Esqueci minha senha</a>
+          <button id="button-blue-login-big" type="submit">
+            Entrar
+          </button>
         </form>
       </div>
       <div id="wrapper-button">
-        <a id="text-2-500">Esqueci minha senha</a>
-        <button id="button-blue-login-big">Entrar</button>
         <p id="text-2-400">Ainda não possui conta?</p>
         <button id="button-grey-login-big">Cadastrar</button>
       </div>
