7bd5b5f5572569b42fd3e958fdfde55aae70ec65
Matheus_Gomes <mathgomes007@htomail.com>
2023-03-07 23:00:40 -0300

    added sidebar

--- src/components/Sign/index.tsx
+++ src/components/Sign/index.tsx
@@ -8,7 +8,7 @@ export const BlueSign = () =>{
             <motion.div className="mainDiv"
             initial={{x: -1200}}
             animate={{x:-10}}
-            transition={{delay: 0.2}}
+            transition={{delay: 0.5}}
             >
                 <h1>Velocidade e experiência em um lugar feito para você</h1>
                 <p>Um ambiente feito para você explorar o seu melhor</p>
