990fc4cc6214178063149324a9d56eb17ffa85cc
Sofia Alexandrino Lage <sofiaalage@gmail.com>
2023-03-09 10:38:54 -0300

    fix:globalStyle

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
-@import url("https://fonts.googleapis.com/css2?family=Lexend:wght@500;600;700&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Lexend:wght@500;600;700&display=swap");
 :root {
   background-color: var( --grey-8);
   overflow-x: hidden;
